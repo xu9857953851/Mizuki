f250fddc7454e7ea13b2a4cf1bf82229482854f1
xu9857953851 <95112711+xu9857953851@users.noreply.github.com>
2025-12-11 17:23:34 +0800

    Delete public/H012 directory

--- public/H012/src/settings.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","Tutorial","TTDes"],collisionMatrix:[[true],[false,false],[false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/start_scene.fire",orientation:"portrait",jsList:[]};
